be9a92f0ce6dfc1622295fd7b5291cf58863188a
FTCMaelstrom3846 <ramseyh9@gmail.com>
2017-10-04 16:51:08 -0400

    Added Minbot Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MinibotTeleop.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.hardware.Hardware;
+
+@TeleOp(name = "Test Teleop")
+public class MinibotTeleop extends OpMode {
+
+    Hardware robot = new Hardware();
+    public void init() {
+
+        robot.init(hardwareMap);
+    }
+
+    public void loop() {
+        robot.leftMotor.setPower(gamepad1.left_stick_y);
+        robot.rightMotor.setPower(-gamepad1.right_stick_y);
+
+        if (gamepad1.left_bumper) {
+            robot.rightGripper.setPosition(0.25);
+            robot.leftGripper.setPosition(0.75);
+        }
+
+        if (gamepad1.right_bumper) {
+            robot.rightGripper.setPosition(0.5);
+            robot.leftGripper.setPosition(0.5);
+        }
+
+    }
+}
