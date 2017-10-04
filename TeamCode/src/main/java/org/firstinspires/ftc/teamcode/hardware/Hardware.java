948dc8e440f2c476e3ed1f8e7d6dec35d9ae7191
FTCMaelstrom3846 <ramseyh9@gmail.com>
2017-10-04 16:52:18 -0400

    Added Minbot Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Hardware.java
@@ -2,23 +2,30 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Ramsey on 10/15/2016.
  */
 
 public class Hardware {
+
     public DcMotor leftMotor;
     public DcMotor rightMotor;
 
+    public Servo rightGripper;
+    public Servo leftGripper;
+
     HardwareMap hwMap;
 
+    public void init(HardwareMap hardwareMap) {
+        this.hwMap = hardwareMap;
 
-    public void init(HardwareMap hwMap) {
-        this.hwMap = hwMap;
+        leftMotor = hwMap.dcMotor.get("leftMotor");
+        rightMotor = hwMap.dcMotor.get("rightMotor");
 
-        leftMotor = hwMap.dcMotor.get("L motor");
-        rightMotor = hwMap.dcMotor.get("R motor");
+        rightGripper = hwMap.servo.get("rightGripperServo");
+        leftGripper = hwMap.servo.get("leftGripperServo");
 
     }
 
