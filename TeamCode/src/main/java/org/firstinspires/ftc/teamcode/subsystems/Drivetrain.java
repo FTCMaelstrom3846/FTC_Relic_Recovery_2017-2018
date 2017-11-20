5c091bfea68ca296293c8811ccd8db820762e684
FTCMaelstrom3846 <ramseyh9@gmail.com>
2017-11-20 16:12:20 -0500

    Tuned autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -60,7 +60,6 @@ public class Drivetrain implements Constants {
         this.teleopAngle = angle;
 
         double speedMagnitude = Math.hypot(x, y); //Ankit is a homosexual
-//agree- Ankit
 /*
         double yComponent = angle == 0 || angle == Math.PI/2 || angle == Math.PI || angle == -Math.PI/2 ? (Math.sin(adjustedAngle)/Math.abs(Math.sin(adjustedAngle))) : Math.sin(adjustedAngle);
         double xComponent = angle == 0 || angle == Math.PI/2 || angle == Math.PI || angle == -Math.PI/2 ? (Math.cos(adjustedAngle)/Math.abs(Math.cos(adjustedAngle))) : Math.cos(adjustedAngle);
